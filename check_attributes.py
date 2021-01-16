cfd17f89900cd973b1ccee9a883573f81f84be02
Scato Eggen <s.j.eggen@umcutrecht.nl>
2021-01-16 01:20:56 +0100

    Use JSON schema to perform basic checks on attributes. Finish basic schema for Characters (text field is still work in progress).

--- check_attributes.py
+++ check_attributes.py
@@ -1,79 +1,199 @@
+import inspect
+import json
 import re
+import textwrap
 from os import system
+
+from jsonschema import validate, ValidationError
+import yaml
 
 from meccg.jsonl import read_all_jsonl
 
 
-system("python.exe load2.py")
+def check_all(attributes):
+    for attribute in attributes:
+        patterns = []
+        with open(f'var/regex/{attribute}.txt', encoding='UTF-8') as fp:
+            for pattern in fp:
+                patterns.append(f'^{pattern.strip()}$')
+
+        passed = 0
+        total = 0
+        example = None
+        for card in read_all_jsonl():
+            if attribute not in card or card[attribute] is None:
+                continue
+            elif attribute in ('skills', 'home_site', 'text'):
+                total += 1
+                if isinstance(card[attribute], list):
+                    if all(
+                        any(re.match(pattern, value) for pattern in patterns)
+                        for value in card[attribute]
+                    ):
+                        passed += 1
+                    elif example is None:
+                        first_mismatch = next(
+                            value
+                            for value in card[attribute]
+                            if not any(re.match(pattern, value) for pattern in patterns)
+                        )
+                        example = f'{card[attribute]} contains invalid item {first_mismatch} ({card["set"]}, {card["category"]})'
+                elif example is None:
+                    example = f'{card[attribute]} is not a list ({card["set"]}, {card["category"]})'
+            else:
+                total += 1
+                if any(re.match(pattern, card[attribute]) for pattern in patterns):
+                    passed += 1
+                elif example is None:
+                    example = f'{card[attribute]} ({card["set"]}, {card["category"]})'
+
+        print(f'{attribute} {passed}/{total}')
+
+        if example is not None:
+            print(f'there were failures for attribute "{attribute}"')
+            print(f'example: {example}')
+            exit(1)
 
 
-def is_required(attribute, card):
-    if attribute in ['name', 'set', 'type', 'alignment']:
-        return True
-    elif attribute in ['class']:
-        return card['type'] in ['Hazard', 'Resource']
-    elif attribute in ['skills']:
-        return card['type'] in ['Character']
-    elif attribute in ['site_type', 'region']:
-        return card['type'] in ['Site']
-    elif attribute in ['region_type']:
-        return card['type'] in ['Region']
+def print_all(attribute):
+    values = sorted(set(
+        value
+        for card in read_all_jsonl()
+        if attribute in card and card[attribute] is not None
+        for value in (card[attribute] if isinstance(card[attribute], list) else [card[attribute]])
+    ))
+    for value in values:
+        print(value)
+
+
+def check_schema(filename):
+    with open(filename) as fp:
+        schema = json.load(fp)
+
+    for card in read_all_jsonl():
+        try:
+            validate(instance=card, schema=schema)
+        except ValidationError as error:
+            print(yaml.dump(card, Dumper=yaml.Dumper, allow_unicode=True))
+            print(error)
+            exit(1)
+
+
+def check_rule(rule, cards):
+    rule_string = inspect.getsource(rule)
+    rule_string = textwrap.dedent(rule_string)
+    rule_string = re.sub(r'^\s*lambda cards: (.*),\s*$', r'\1', rule_string, flags=re.DOTALL)
+    print(rule_string, end='... ')
+    last_card = None
+    result = rule((last_card := card) for card in cards)
+    if result:
+        print('OK')
     else:
-        return False
+        print('FAIL')
+        print()
+        print(yaml.dump(last_card, Dumper=yaml.Dumper, allow_unicode=True))
+    return result
 
 
-def is_nullable(attribute):
-    return attribute not in ['set', 'category', 'type', 'site_type', 'region', 'region_type']
+def check_rules(rules):
+    cards = list(read_all_jsonl())
+    for rule in rules:
+        result = check_rule(rule, cards)
+        if not result:
+            exit(1)
 
+
+RULES = [
+    # CHARACTERS
+    lambda cards: all(
+        card['mp'] == ''
+        for card in cards
+        if card['type'] == 'Character' and card['race'] in ('Balrog', 'Ringwraith', 'Fallen-wizard', 'Wizard')
+    ),
+    lambda cards: all(
+        card['mp'] == ''
+        for card in cards
+        if card['type'] == 'Character' and card['mind'] in ('1', '2')
+    ),
+    lambda cards: all(
+        card['mp'] == '1'
+        for card in cards
+        if card['type'] == 'Character' and card['mind'] in ('3', '4')
+    ),
+    lambda cards: all(
+        card['mp'] == '2'
+        for card in cards
+        if card['type'] == 'Character' and card['mind'] in ('5', '6', '7')
+    ),
+    lambda cards: all(
+        card['mp'] == '3'
+        for card in cards
+        if card['type'] == 'Character' and card['mind'] in ('8', '9', '10')
+    ),
+    lambda cards: all(
+        card['mind'] is None
+        for card in cards
+        if card['type'] == 'Character' and card['race'] in ('Balrog', 'Ringwraith', 'Fallen-wizard', 'Wizard')
+    ),
+    lambda cards: all(
+        card['mind'] is not None
+        for card in cards
+        if card['type'] == 'Character' and card['race'] not in ('Balrog', 'Ringwraith', 'Fallen-wizard', 'Wizard')
+    ),
+    lambda cards: all(
+        card['gi'] is not None
+        for card in cards
+        if card['type'] == 'Character' and card['race'] == 'Fallen-wizard'
+    ),
+    lambda cards: all(
+        card['gi'] is None
+        for card in cards
+        if card['type'] == 'Character' and card['race'] != 'Fallen-wizard'
+    ),
+    lambda cards: all(
+        card['di'] in ('0', '1', '2', '3', '4')
+        for card in cards
+        if card['type'] == 'Character' and card['race'] not in ('Balrog', 'Ringwraith', 'Fallen-wizard', 'Wizard')
+    ),
+    lambda cards: all(
+        card['di'] in ('3', '4', '5', '6')
+        for card in cards
+        if card['type'] == 'Character' and card['race'] in ('Balrog', 'Ringwraith')
+    ),
+    lambda cards: all(
+        card['di'] in ('5', '7', '9', '10', '12')
+        for card in cards
+        if card['type'] == 'Character' and card['race'] in ('Fallen-wizard', 'Wizard')
+    ),
+    lambda cards: all(
+        len(card['skills']) == 2
+        for card in cards
+        if card['type'] == 'Character' and card['race'] == 'Balrog'
+    ),
+    lambda cards: all(
+        len(card['skills']) in (1, 2, 3)
+        for card in cards
+        if card['type'] == 'Character' and card['race'] == 'Ringwraith'
+    ),
+    lambda cards: all(
+        len(card['skills']) == 4
+        for card in cards
+        if card['type'] == 'Character' and card['race'] in ('Fallen-wizard', 'Wizard')
+    ),
+]
 
 ATTRIBUTES = [
     'set', 'category',
     'type', 'alignment',
     'mp', 'site_type', 'region_type', 'name',
-    'class', 'race', 'skills', 'region',
+    'mind', 'gi', 'di',
+    'skills', 'race', 'class', 'region',
+    'prowess', 'body', 'home_site', 'cp',
+    'text',
 ]
 
-for attribute in ATTRIBUTES:
-    patterns = []
-    with open(f'var/regex/{attribute}.txt', encoding='UTF-8') as fp:
-        for pattern in fp:
-            patterns.append(f'^{pattern.strip()}$')
-
-    passed = 0
-    total = 0
-    example = None
-    for card in read_all_jsonl():
-        if attribute not in card:
-            if is_required(attribute, card):
-                total += 1
-                if example is None:
-                    example = f'attribute is missing ({card["set"]}, {card["category"]})'
-        elif card[attribute] is None:
-            if not is_nullable(attribute):
-                total += 1
-                if example is None:
-                    example = f'attribute is None ({card["set"]}, {card["category"]})'
-        elif attribute == 'skills':
-            total += 1
-            if isinstance(card[attribute], list):
-                if all(
-                    any(re.match(pattern, skill) for pattern in patterns)
-                    for skill in card[attribute]
-                ):
-                    passed += 1
-                elif example is None:
-                    example = f'{card[attribute]} contains invalid item ({card["set"]}, {card["category"]})'
-            elif example is None:
-                example = f'{card[attribute]} is not a list ({card["set"]}, {card["category"]})'
-        else:
-            total += 1
-            if any(re.match(pattern, card[attribute]) for pattern in patterns):
-                passed += 1
-            elif example is None:
-                example = f'{card[attribute]} ({card["set"]}, {card["category"]})'
-
-    print(f'{attribute} {passed}/{total}')
-
-    if example is not None:
-        print(f'there were failures for attribute "{attribute}"')
-        print(f'example: {example}')
+# system("python.exe load2.py")
+# check_schema('var/schema.json')
+# print_all('text')
+check_all(ATTRIBUTES)
+# check_rules(RULES)
--- load2.py
+++ load2.py
@@ -1,12 +1,13 @@
+import re
+
 import yaml
 
 from lib.meccg.scraping import load_html
 from lib.meccg.jsonl import dump_jsonl
 from lib.meccg.unjinja import untemplate
 
-if __name__ == '__main__':
-    # template_whitelist = None
-    template_whitelist = ('wiz.jinja',)
+
+def dump_all(template_whitelist=None):
     file_blacklist = ('atscreatnew.html', 'empty.html', 'german.html')
     two_letter_template_names = ('dm', 'le', 'wh')
 
@@ -34,8 +35,9 @@ if __name__ == '__main__':
             print(f'Skipping {file_name}')
             continue
 
-        if file_name in ('lefactions.html', 'leitems.html', 'lemevents.html', 'leminions.html'):
-            encoding = 'mac_latin2'
+        if file_name in ('lecreatures.html', 'lefactions.html', 'leitems.html', 'lemevents.html', 'leminions.html',
+                         'leringwraith.html'):
+            encoding = 'mac_roman'
 
         print(f'loading {file_name} using {template_name}')
 
@@ -74,4 +76,64 @@ if __name__ == '__main__':
                 for card in set['cards']
             ]
 
+        for card in cards:
+            if card['name'] == 'Dôgrib':
+                card['mp'] = ''
+                card['mind'] = '2'
+
+            if card['name'] == 'Bill Ferny' and card['category'] == 'Promo Cards':
+                card['prowess'] = '2'
+                card['body'] = '8'
+
+            if card['name'] == 'Barrow-wight' and card['set'] == 'The Lidless Eye':
+                card['body'] = '-'
+
+            if card['name'] == 'Goblin-faces':
+                card['body'] = '-'
+
+            if card['name'] == 'The White Hand':
+                card['body'] = '-'
+
+            if card['type'] == 'Character' or card['type'] == 'Hazard' and card['class'] == 'Agent':
+                if 'mp' not in card or card['mp'] is None or card['mp'] == '0':
+                    card['mp'] = ''
+                if 'mind' not in card or card['mind'] == '' or card['mind'] == '0':
+                    card['mind'] = None
+                if 'gi' not in card:
+                    card['gi'] = None
+                if card['set'] == 'Dark Minions' and card['category'] != 'Promo Cards':
+                    card['home_site'] = [
+                        home_site.replace('\n', ' ').strip()
+                        for home_site in card['home_site'].split(',')
+                    ]
+                elif card['set'] == 'The White Hand':
+                    card['home_site'] = [
+                        home_site.replace('\n', '')
+                        for home_site in card['home_site']
+                    ]
+                if 'cp' not in card or card['cp'] == '' or card['cp'] == '0':
+                    card['cp'] = None
+                if isinstance(card['text'], list):
+                    card['text'] = ''.join(card['text'])
+                card['text'] = card['text'].replace('\n', ' ')
+                card['text'] = card['text'].replace('  ', ' ')
+                card['text'] = re.sub(r'(\."?) *', r'\1\n', card['text'])
+                card['text'] = card['text'].split('\n')
+                if card['text'][-1] == '':
+                    card['text'] = card['text'][:-1]
+                else:
+                    card['text'][-1] += '.'
+            else:
+                if 'mind' in card and card['mind'] in ('', '0'):
+                    card['mind'] = None
+                if 'cp' in card and card['cp'] in ('', '0'):
+                    card['cp'] = None
+                if 'text' in card:
+                    # TODO: remove this in the end; for now it's just to make validation easier
+                    card['text'] = None
+
         dump_jsonl(f'var/jsonl/{base_name}.jsonl', cards)
+
+
+dump_all()
+# dump_all(('bal.jinja',))
